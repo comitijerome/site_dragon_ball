89704698b7c54190ea697e2e7a417e0897f7e17b
jerome <jcomiti@gmail.com>
2025-08-04 22:57:47 +0200

    alléger le Layout

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -1,2 +1,5 @@
-<h1><?= $data->title ?></h1>
-<p>homepage accueil</p>
+<p>Bienvenue sur le site de Dragon Ball !</p>
+<p>Explorez l'univers de Dragon Ball, découvrez les personnages emblématiques et leurs techniques légendaires.</p>
+<p>Rejoignez-nous pour des aventures épiques et des combats mémorables !</p>
+<p>Que la force de Dragon Ball soit avec vous !</p>
+<p>Pour toute question ou suggestion, n'hésitez pas à nous contacter.</p>
--- Views/base.php
+++ Views/base.php
@@ -21,28 +21,13 @@
                 <li><a href="#">Techniques</a></li>
             </ul>
         </nav>
-
     </header>
     <div class="container">
         <main>
             <?= $content ?>
 
-            <p>Bienvenue sur le site de Dragon Ball !</p>
-            <p>Explorez l'univers de Dragon Ball, découvrez les personnages emblématiques et leurs techniques légendaires.</p>
-            <p>Rejoignez-nous pour des aventures épiques et des combats mémorables !</p>
-            <p>Que la force de Dragon Ball soit avec vous !</p>
-            <p>Pour toute question ou suggestion, n'hésitez pas à nous contacter.</p>
 
         </main>
-        <aside>
-            <p>Rejoignez-nous pour des aventures épiques !</p>
-            <p>Que la force de Dragon Ball soit avec vous !</p>
-            <p> n'hésitez pas à nous contacter.</p>
-        </aside>
-    </div>
-    <footer>
-        2025
-    </footer>
 </body>
 
 </html>
