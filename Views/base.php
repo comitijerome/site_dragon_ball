1e5191ff3d818603d5976d2ec1e3033dccd1ac8c
jerome <jcomiti@gmail.com>
2025-08-05 00:54:07 +0200

    correction css

--- Views/base.php
+++ Views/base.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <title><?= $data->title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Feuille de style CSS -->
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
 </head>
-
 <body>
-    <header>
-        <h1>dragon ball</h1>
-
-        <nav>
+    <div id="root">
+        <header id="global-layout-header">
+            <h1>dragon ball</h1>
+        </header>
+        <nav id="global-layout-nav">
             <ul>
                 <li><a href="#">Accueil</a></li>
                 <li><a href="#">Dragon world</a></li>
@@ -21,13 +21,11 @@
                 <li><a href="#">Techniques</a></li>
             </ul>
         </nav>
-    </header>
-    <div class="container">
-        <main>
-            <?= $content ?>
-
-
-        </main>
+        <div class="container">
+            <main id="global-layout-main">
+                <?= $content?>
+            </main>
+        </div>
+    </div>
 </body>
-
 </html>
